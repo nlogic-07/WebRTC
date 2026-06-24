3fe92c2f7fe5abc010efe2f6f8517c97bde69399
nlogic-07 <choprajaskaran76@gmail.com>
2026-06-24 23:19:06 +0530

    basic ui & show/stop video

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const videoEl = document.querySelector("#my-video");
+
 let stream = null; //Init stream var to use stream anywhere
 const constraints = {
   audio: true,
@@ -8,11 +10,65 @@ const getMicAndCamera = async (e) => {
   try {
     stream = await navigator.mediaDevices.getUserMedia(constraints);
     console.log(stream);
-  } catch {
+    changeButtons([
+      "green",
+      "blue",
+      "blue",
+      "grey",
+      "grey",
+      "grey",
+      "grey",
+      "grey",
+    ]);
+  } catch (err) {
     console.log("User denied access to constraints");
+    console.log(err);
   }
+};
+
+const showMyFeed = (e) => {
+  console.log("showMyFeed is working");
+  videoEl.srcObject = stream; //Setting our mediastream(stream) to our <video/>
+  const tracks = stream.getTracks();
+  console.log(tracks);
+  changeButtons([
+    "green",
+    "green",
+    "blue",
+    "blue",
+    "blue",
+    "grey",
+    "grey",
+    "blue",
+  ]);
+};
+
+const stopMyFeed = (e) => {
+  const tracks = stream.getTracks();
+  tracks.forEach((track) => {
+    // console.log(track);
+    track.stop(); //disassociates the track with the source
+  });
+  changeButtons([
+    "blue",
+    "grey",
+    "grey",
+    "grey",
+    "grey",
+    "grey",
+    "grey",
+    "grey",
+  ]);
 };
 
 document
   .querySelector("#share")
   .addEventListener("click", (e) => getMicAndCamera(e));
+
+document.querySelector("#show-video").addEventListener("click", (e) => {
+  showMyFeed(e);
+});
+
+document.querySelector("#stop-video").addEventListener("click", (e) => {
+  stopMyFeed(e);
+});
