d6c9c078c5a94288e8f514355cf8d4a6df5aa290
nlogic-07 <choprajaskaran76@gmail.com>
2026-06-28 16:09:36 +0530

    basic ui changes

--- screenRec.js
+++ screenRec.js
@@ -2,6 +2,10 @@ let mediaRecorder;
 let recordedBlobs;
 
 const startRecording = (e) => {
+  if (!stream) {
+    alert("No current feed");
+    return;
+  }
   console.log("start recording");
   recordedBlobs = [];
   mediaRecorder = new MediaRecorder(stream);
@@ -10,18 +14,58 @@ const startRecording = (e) => {
     recordedBlobs.push(e.data);
   };
   mediaRecorder.start();
+
+  changeButtons([
+    "green",
+    "green",
+    "blue",
+    "blue",
+    "green",
+    "blue",
+    "grey",
+    "blue",
+  ]);
 };
 
 const stopRecording = (e) => {
+  if (!mediaRecorder) {
+    alert("Please record before stopping");
+    return;
+  }
   console.log("stop recording");
   mediaRecorder.stop();
+  changeButtons([
+    "green",
+    "green",
+    "blue",
+    "blue",
+    "green",
+    "green",
+    "blue",
+    "blue",
+  ]);
 };
 
 const playRecording = (e) => {
   console.log("play recording");
+  if (!recordedBlobs) {
+    alert("No recording saved");
+    return;
+  }
   const superBuffer = new Blob(recordedBlobs);
   const recorderVideoEl = document.querySelector("#other-video");
   recorderVideoEl.src = window.URL.createObjectURL(superBuffer);
   recorderVideoEl.controls = true;
   recorderVideoEl.play();
+
+  changeButtons([
+    "green",
+    "green",
+    "blue",
+    "blue",
+    "green",
+    "green",
+    "green",
+    "blue",
+  ]);
 };
